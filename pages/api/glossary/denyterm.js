c31b51019aea18832ca0b9e93f86fe6916962b8b
Angel Lopez Pol <59091763+angel1254mc@users.noreply.github.com>
2022-08-18 12:16:57 -0400

    added super secret server API key to admin funcs

--- pages/api/glossary/denyterm.js
+++ pages/api/glossary/denyterm.js
@@ -1,5 +1,6 @@
 
 export default async (req, res) => {
+    req.body.SERVER_SECRET_API_KEY = process.env.SERVER_SECRET_API_KEY;
     const parseBody = JSON.parse(req.body)
     const response =  await fetch('https://get-server-prod.herokuapp.com/glossary/denyrequested', {
         credentials: 'include',
--- pages/api/glossary/updateterm.js
+++ pages/api/glossary/updateterm.js
@@ -1,6 +1,7 @@
 
 
 export default async (req, res) => {
+    req.body.SERVER_SECRET_API_KEY = process.env.SERVER_SECRET_API_KEY;
     const parseBody = JSON.parse(req.body)
     const response =  await fetch('https://get-server-prod.herokuapp.com/glossary/updateterm', {
         credentials: 'include',
