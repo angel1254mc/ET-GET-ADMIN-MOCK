aea3cdc05ac25e2c69ece65b94c8325b3903c0d4
CarlosRios-ET <110127686+CarlosRios-ET@users.noreply.github.com>
2022-08-18 20:18:14 -0400

    Add files via upload

--- pages/admin/settings.js
+++ pages/admin/settings.js
@@ -1,8 +1,53 @@
 import React from 'react'
-
+import styles from '../../styles/settings.module.css'
+import Sidebar from '../../src/components/Sidebar.js'
 function settings({}) {
   return (
-    <div>settings</div>
+    <div className={styles.body}>
+      <Sidebar></Sidebar>
+      <div className={styles.title}>
+        Settings
+      </div>
+      <div className={styles.admin_text}>
+        Admin Accounts:
+      </div>
+      <div className={styles.flex_button_container}>
+        <div className={styles.button1}>
+          <button>Add +</button>
+        </div>
+      </div>
+        <div className={styles.login}>
+          <label>Username:</label>
+          <label className={styles.password}>Password:</label>
+        </div>
+        <div className={styles.login_input}>
+          <input className={styles.username_input} type='text' placeholder='Enter username' name='username' required></input>
+          <input className={styles.password_input} type='text' placeholder='Enter password' name='password' required></input>
+        </div>
+        <div className={styles.logout}>
+          Logout?
+        </div>
+          <button className={styles.button2}>Logout</button>
+        <div>
+          <select className={styles.dropdown}><option>Choose an account: </option>
+          <option>Admin Placeholder 1</option><option>Admin Placeholder 2</option>
+          <option>Admin Placeholder 3</option></select>
+        </div>
+        <div className={styles.temp_checklist}>
+          <div className={styles.read_option}>
+            <label htmlFor='read only'>Read: </label>
+            <input type='checkbox' name='read only'></input>
+          </div>
+          <div className={styles.edit_option}>
+            <label htmlFor='edit'>Edit: </label>
+            <input type='checkbox' name='edit'></input>
+          </div>
+          <div className={styles.delete_option}>
+            <label htmlFor='delete'>Delete: </label>
+            <input type='checkbox' name='delete'></input>
+          </div>
+        </div>
+    </div>
   )
 }
 const authMiddleware = (req) => {
